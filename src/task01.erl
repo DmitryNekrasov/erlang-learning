c350f70648319cce5e8c50eb218bd4b4f9d3641d
Dmitry <scannorone@gmail.com>
2017-01-27 10:20:43 +0700

    task01

--- src/task01.erl
+++ src/task01.erl
@@ -3,4 +3,9 @@
 
 -export([main/0]).
 
-main() -> erlang:display(hello).
+makeList(0) -> [];
+makeList(N) -> makeList(N div 10) ++ [N rem 10].
+
+main() ->
+  L = makeList(1234567890),
+  erlang:display(L).
