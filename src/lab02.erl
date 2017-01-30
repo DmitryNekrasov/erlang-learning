0b8e606bfef0fec2eb54b2cb7762d94efc861959
Dmitry <scannorone@gmail.com>
2017-01-30 14:24:25 +0700

    Вывод, вердикт от контроллера (lab02)

--- src/lab02.erl
+++ src/lab02.erl
@@ -12,42 +12,64 @@ teamAction() ->
 
 team(PidClientApp) ->
   TeamAction = teamAction(),
-  PidClientApp ! {team, self(), TeamAction},
+  if
+    TeamAction == send_task ->
+      ProblemId = rand:uniform(100),
+      io:format("Team submit Problem ~w~n", [ProblemId]),
+      PidClientApp ! {team, self(), ProblemId};
+    true ->
+      io:format("Team check monitor~n"),
+      PidClientApp ! {team, self()}
+  end,
   receive
-    client_app -> io:format("(Team) Client App received~n")
+    client_app -> io:format("Team get monitor~n")
   end.
 
 utility(PidMonitor, PidTeam) ->
+  io:format("Client App requested monitor~n"),
   PidMonitor ! {client_app, self()},
   receive
     monitor ->
-      io:format("(ClientApp) Monitor received~n"),
+      io:format("Client App get monitor~n"),
+      io:format("Client App send monitor to team~n"),
       PidTeam ! client_app
   end.
 
 clientApp(PidMonitor, PidController) ->
   receive
-    {team, PidTeam, get_monitor} ->
+    {team, PidTeam} ->
       utility(PidMonitor, PidTeam);
-    {team, PidTeam, send_task} ->
-      PidController ! {client_app, PidMonitor},
+    {team, PidTeam, ProblemId} ->
+      io:format("Client App send problem ~w~n", [ProblemId]),
+      PidController ! {client_app, PidMonitor, ProblemId},
       utility(PidMonitor, PidTeam)
   end.
 
 monitor() ->
   receive
     {client_app, ClientAppPid} ->
-      io:format("(Monitor) Client App received~n"),
+      io:format("Monitor returned to client app~n"),
       ClientAppPid ! monitor;
-    controller ->
-      io:format("(Monitor) Controller received~n")
+    {controller, ProblemId, Verdict} ->
+      io:format("Monitor get ~s (Problem ~w)~n", [Verdict, ProblemId])
+  end.
+
+verdict() ->
+  Rand = rand:uniform(5),
+  if
+    Rand == 1 -> "AC";
+    Rand == 2 -> "WA";
+    Rand == 3 -> "TL";
+    Rand == 4 -> "ML";
+    true -> "RE"
   end.
 
 controller() ->
   receive
-    {client_app, PidMonitor} ->
-      io:format("(Controller) Client App received~n"),
-      PidMonitor ! controller
+    {client_app, PidMonitor, ProblemId} ->
+      io:format("Controller get Problem ~w~n", [ProblemId]),
+      io:format("Controller update monitor~n"),
+      PidMonitor ! {controller, ProblemId, verdict()}
   end.
 
 for(Callback, 1) -> Callback();
@@ -56,7 +78,7 @@ for(Callback, N) ->
   for(Callback, N - 1).
 
 main() ->
-  N = 2,
+  N = 4,
   PidMonitor = spawn(fun() -> for(fun() -> monitor() end, N) end),
   PidController = spawn(fun() -> for(fun() -> controller() end, N) end),
   PidClientApp = spawn(fun() -> for(fun() -> clientApp(PidMonitor, PidController) end, N) end),
